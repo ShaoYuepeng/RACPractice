b804963cb2701b0ac19c5055781790eeb120c136
Y了个P <Shao_Yuepeng@163.com>
2018-04-06 15:45:30 +0800

    Initial Commit

---
+++ RAC练习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RAC练习
+//
+//  Created by 邵岳鹏 on 2018/4/6.
+//  Copyright © 2018年 邵岳鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ RAC练习Tests/RAC__Tests.m
@@ -0,0 +1,39 @@
+//
+//  RAC__Tests.m
+//  RAC练习Tests
+//
+//  Created by 邵岳鹏 on 2018/4/6.
+//  Copyright © 2018年 邵岳鹏. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RAC__Tests : XCTestCase
+
+@end
+
+@implementation RAC__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
